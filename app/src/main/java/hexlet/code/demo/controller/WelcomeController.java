3bba89f9c3f391fc7bff2201896d070ed6ae2318
e4riya <kotkovdima0@gmail.com>
2026-03-21 00:25:02 +0300

    add application.yaml and welcome controller with value from application.yaml

---
+++ app/src/main/java/hexlet/code/demo/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package hexlet.code.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @Value("${app.welcome-message}")
+    private String welcomeMessage;
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return welcomeMessage;
+    }
+}
